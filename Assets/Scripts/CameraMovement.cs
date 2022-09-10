091f9815c494539e7da36c8e2bf5374bf2af7921
PaladinoSensetivo <igorpannek.gamedev@gmail.com>
2022-09-09 21:43:50 -0300

    Update
    Adptado codigo de movimentacao de camera para mobile, agregado giroscopio e adicionado TMP para inclusao de texto no botao.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,15 +9,15 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]private TouchCamera touchCamera;
     private Transform currentPos;
     private Vector3 velocity = Vector3.zero;
-    private bool isMoving;
+    private bool isMoving, isReturning, gyroEnabled;
 
-    public bool gyroEnabled;
     private Gyroscope gyro;
 
     private GameObject cameraContainer;
 
+    RaycastHit hit;
+
     private void Start() {
-        //Criando uma parent para a camera, para poder mover a camera e definir a rotacao.
         cameraContainer = new GameObject("Camera Container");
         cameraContainer.transform.position = transform.position;
         transform.SetParent(cameraContainer.transform);
@@ -25,69 +25,77 @@ public class CameraMovement : MonoBehaviour
 
 	void Update()
     {
-        //chamando o metodo
-        MoveCamera();
 
-        //chamando o metodo que retorna uma bool e atualizar minha rotacao da camera conforme a valor que retorna.
-        //verdadeiro retorna a toracao do giroscopio, falso retorna uma rotacao ja pre definida
         if(gyroEnabled) {
             transform.localRotation = GyroToUnity(gyro.attitude);
             Debug.Log("Gyro on");
         }
-        else{
-            transform.rotation = Quaternion.Euler(30f, 45f, 0f);
-            Debug.Log("Gyro off");
+        MoveCamera();
+
+        if(Input.touchCount > 0) {
+
+		    Touch touch = Input.GetTouch(0);
+		    Vector3 pos = touch.position;
+
+            if(touch.phase == TouchPhase.Began) {
+                Ray ray = Camera.main.ScreenPointToRay(pos);
+
+                if(Physics.Raycast(ray, out hit)) {
+                    if(hit.collider.tag == "Cabinet") {
+                        isMoving = true;
+                        currentPos = target;
+                        touchCamera.enabled = false;
+                        gyroEnabled = EnableGyro();
+
+                        cameraContainer.transform.rotation = target.rotation;
+
+                    }
+			    }
+		    }
 		}
     }
 
     void MoveCamera() {
-		#region Inputs
-		if(Input.GetKeyDown(KeyCode.Space)) {
-            isMoving = true;                    //tornando a variavel verdadeira para dar o trigger na movimentacao
-            currentPos = target;                //definindo o destino da minha camera
-            touchCamera.enabled = false;        //desligando o script responsavel pela movimentacao do cenario
-            gyroEnabled = EnableGyro();         //chamando o metodo para atualizar o valor que ele deve retornar
-        }
 
-
-        if(Input.GetKeyDown(KeyCode.Backspace)) {
-            isMoving = true;
-            currentPos = start;
-            touchCamera.enabled = true;
-            cameraContainer.transform.rotation = currentPos.rotation;       //retornando o parent para a rotacao inicial
-            gyroEnabled = EnableGyro();
-        }
-		#endregion
-
-        //animacao da movimentacao da camera caso a viariavel seja verdadeira e sua posicao atual seja diferente da posicao desejada
-		#region Position
 		if(isMoving && cameraContainer.transform.position != currentPos.position ) {
             cameraContainer.transform.position = Vector3.SmoothDamp(cameraContainer.transform.position, currentPos.position, ref velocity, speed * Time.deltaTime);     
         }
 		else {
             isMoving = false;
         }
-        #endregion
+
+    }
+
+    public void ReturnCamera() {
+        isMoving = true;
+        isReturning = true;
+        currentPos = start;
+        touchCamera.enabled = true;
+        cameraContainer.transform.rotation = currentPos.rotation;
+        gyroEnabled = EnableGyro();
     }
 
     private bool EnableGyro() {
-        if(Input.GetKeyDown(KeyCode.Space)) {       //se o touch detectar a tag liga o giroscopio e faz com que o parent tenha a mesma rotacao do destino
-            gyro = Input.gyro;
-            gyro.enabled = true;
+        if(hit.collider != null) {
 
-            cameraContainer.transform.rotation = target.rotation;
+            if(hit.collider.tag == "Cabinet") {      
+                gyro = Input.gyro;
+                gyro.enabled = true;
 
+                
 
-            return true;
-        }
-        else if(Input.GetKeyDown(KeyCode.Backspace)) {      //ao clicar botao de retornar desliga o giroscopio e retorna falso para manter a camera na sua posicao e rotacao original
+                return true;
+            }
+		}
+        else if(isReturning == true) {      
             gyro.enabled = false;
+            isReturning = false;
+            transform.rotation = Quaternion.Euler(30f, 45f, 0f);
             return false;
 		}
         return false;
     }
 
-    //metodo responsavel pela calibracao do giroscopio
     private Quaternion GyroToUnity(Quaternion q) {
         return new Quaternion(q.x,
             q.y,
